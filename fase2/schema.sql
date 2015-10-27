742a8a86f9705777ce92823e52131ba95a670871
Vinicius <vinicius.nas.silva@gmail.com>
2015-10-27 13:17:34 -0200

    Atualizado restrições em chaves estrangeiras

--- fase2/schema.sql
+++ fase2/schema.sql
@@ -1,28 +1,28 @@
-create table membros(
-  email varchar(254) primary key,
-  senha char(32) not null,
-  nome varchar(255),
-  login varchar(20) unique not null,
-  data_criacao date not null
-);
+  create table membros(
+    email varchar(254) primary key,
+    senha char(32) not null,
+    nome varchar(255),
+    login varchar(20) unique not null,
+    data_criacao date not null
+  );
 
-insert into membros(
-  email,
-  senha,
-  nome,
-  login,
-  data_criacao
-) values (
-  'admin@gelic.com.br',
-  md5('mudeme'),
-  'Administrador',
-  'admin',
-  now()
-);
+  insert into membros(
+    email,
+    senha,
+    nome,
+    login,
+    data_criacao
+  ) values (
+    'admin@gelic.com.br',
+    md5('mudeme'),
+    'Administrador',
+    'admin',
+    now()
+  );
 
-create table administradores(
-  email varchar(254) primary key references membros(email) 
-);
+  create table administradores(
+    email varchar(254) primary key references membros(email) on delete cascade on update cascade
+  );
 
 insert into administradores(
   email
@@ -30,7 +30,7 @@ insert into administradores(
   'admin@gelic.com.br'
 );
 
-alter table membros add email_admin varchar(254) references administradores(email);
+alter table membros add email_admin varchar(254) references administradores(email) on delete set null on update cascade;
 
 update membros set email_admin = 'admin@gelic.com.br' where email = 'admin@gelic.com.br';
 
@@ -43,7 +43,7 @@ create table midias(
   duracao interval not null,
   email_coletor varchar(254),
   data_gravacao date not null,
-  email_admin varchar(254) not null references administradores(email),
+  email_admin varchar(254) not null references administradores(email) on update cascade,
   data_submissao date not null
 );
 
@@ -53,50 +53,50 @@ create table paises(
 
 create table estados(
   nome varchar(50),
-  nome_pais varchar(50) references paises,
+  nome_pais varchar(50) references paises on update cascade,
   constraint estados_pk primary key (nome, nome_pais)
 );
 
 create table cidades(
   nome varchar(50),
-  nome_pais varchar(50) references paises,
+  nome_pais varchar(50) references paises on update cascade,
   nome_estado varchar(50),
   constraint cidades_pk primary key (nome, nome_pais),
-  constraint cidades_estados_fk foreign key (nome_estado, nome_pais) references estados (nome, nome_pais)
+  constraint cidades_estados_fk foreign key (nome_estado, nome_pais) references estados (nome, nome_pais) on delete set null on update cascade
 );
 
 create table transcricoes(
-  id_midia int primary key references midias,
+  id_midia int primary key references midias on delete cascade,
   codigo varchar(100) not null,
   comentario text,
   nome_cidade varchar(50) not null,
   nome_pais varchar(50) not null,
-  constraint transcricoes_cidades_fk foreign key (nome_cidade, nome_pais) references cidades (nome, nome_pais)
+  constraint transcricoes_cidades_fk foreign key (nome_cidade, nome_pais) references cidades (nome, nome_pais) on update cascade
 );
 
 create table audios(
-  id_midia int primary key references midias,
+  id_midia int primary key references midias on delete cascade,
   codigo varchar(100) not null
 );
 
 create table videos(
-  id_midia int primary key references midias,
+  id_midia int primary key references midias on delete cascade,
   link text not null
 );
 
 create table idiomas(
-  id_midia int primary key references midias,
+  id_midia int primary key references midias on delete cascade,
   idioma varchar(50) not null
 );
 
 create table membro_fez_transcricao(
-  email_membro varchar(254) references membros,
+  email_membro varchar(254) references membros on update cascade,
   id_midia int references midias,
   constraint membro_fez_transcricao_pk primary key (email_membro, id_midia)
 );
 
 create table membro_revisou_transcricao(
-  email_membro varchar(254) references membros,
+  email_membro varchar(254) references membros on update cascade,
   id_midia int references midias,
   constraint membro_revisou_transcricao_pk primary key (email_membro, id_midia)
 );
