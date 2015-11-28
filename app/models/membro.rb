bef52367e42b814d0198870f589d499f5bca882a
Vinicius <vinicius.nas.silva@gmail.com>
2015-11-28 14:52:32 -0200

    Obriga a confirmar a senha

--- app/models/membro.rb
+++ app/models/membro.rb
@@ -3,9 +3,22 @@ require 'digest/md5'
 class Membro < ActiveRecord::Base
   self.primary_key = 'email'
 
-  def senha=(value)
-    encrypted = Digest::MD5.hexdigest(value)
-    write_attribute(:senha, encrypted)
+  validates_confirmation_of :senha
+  validates_presence_of :senha_confirmation
+
+  before_save :encrypt_senha
+
+  def encrypt_senha
+    self.senha = Digest::MD5.hexdigest(senha)
+  end
+
+  def self.authenticate(email, password)
+    membro = Membro.find(email)
+    if membro && membro.senha == Digest::MD5.hexdigest(password)
+      membro
+    else
+      nil
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GELICDb::Application.routes.draw do
+  get '/' => 'home#index'
   resources :membros, :id => /[^\/]+/
 
   # The priority is based upon order of creation: first created -> highest priority.
