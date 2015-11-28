5fd68f2fbb5516a4c408c393dd4da45b703835c6
Vinicius <vinicius.nas.silva@gmail.com>
2015-11-28 12:26:12 -0200

    Inicia o Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GELICDb::Application.config.secret_key_base = 'e37c0e1438d3d5fbb0b4260ed0df63c3b6f7851ee9fc936001ae9ac8af8d043628548ced601b4644132240620c19a35cf749d88ad98cff96724f8aa29e683b00'
