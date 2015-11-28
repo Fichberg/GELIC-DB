d0a4d85d0998dc6d51236a52e83ba1ab4dd4737b
Vinicius <vinicius.nas.silva@gmail.com>
2015-11-28 13:38:41 -0200

    guarda a senha em md5

--- app/models/membro.rb
+++ app/models/membro.rb
@@ -1,3 +1,11 @@
+require 'digest/md5'
+
 class Membro < ActiveRecord::Base
   self.primary_key = 'email'
+
+  def senha=(value)
+    encrypted = Digest::MD5.hexdigest(value)
+    write_attribute(:senha, encrypted)
+  end
+
 end
