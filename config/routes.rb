fd758b12dcde6ba4ee05c0194d6bb7421ba4327c
Fichberg <rfichberg@gmail.com>
2015-11-28 15:34:24 -0200

    Midia index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 GELICDb::Application.routes.draw do
   resources :membros, :id => /[^\/]+/
+  resources :midias, :id => /[^\/]+/
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
